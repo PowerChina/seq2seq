4588f9f22661225f14ac04f600cd9ef395b6379b
cesa0820 <liyihannew@126.com>
2016-11-12 11:28:35 +0800

    update conversation.py

--- conversation.py
+++ conversation.py
@@ -10,29 +10,29 @@ from collections import Counter
 from itertools import tee
 import numpy as np
 
-from keras.models import Sequential
-from seq2seq.models import AttentionSeq2seq,SimpleSeq2seq
-
 from keras.layers.recurrent import LSTM
 from keras.layers.embeddings import Embedding
-from keras.layers.core import TimeDistributedDense,Activation
+from keras.layers.core import TimeDistributedDense,Activation,Dropout
+from keras.models import Sequential
+from seq2seq.models import AttentionSeq2seq,SimpleSeq2seq
 from seq2seq.layers.decoders import AttentionDecoder
+from keras.layers import RepeatVector,TimeDistributed,Dense
 
 CORPUS_FILE_PATH = "./data/vectors.bin.skipgram.mergenew.2.3"
 DATA_SET_PATH = "./data/data.txt"
 TRAIN_SET_PATH = "./data/train.txt"
 TEST_SET_PATH = "./data/test.txt"
 TOKEN_REPRESENTATION_SIZE = 300
-INPUT_SEQUENCE_LENGTH = 16
-ANSWER_MAX_TOKEN_LENGTH = 6
+INPUT_SEQUENCE_LENGTH = 20
+ANSWER_MAX_TOKEN_LENGTH = 16
 VOCAB_MAX_SIZE = 20000
 HIDDEN_LAYER_DIMENSION = 512
 FULL_LEARN_ITER_NUM = 500
 SAMPLES_BATCH_SIZE = 100
 EOS_SYMBOL = '$$$'
 EMPTY_SYMBOL= '###'
-TRAIN_BATCH_SIZE = 20
-NN_MODEL_PATH = './model/model_complete'
+TRAIN_BATCH_SIZE = 20 
+NN_MODEL_PATH = './model/model'
 
 '''
     preprocess for sentences
@@ -75,6 +75,7 @@ def get_tokens_voc(tokenized_sentences):
             token_counter.update([token])
     
     token_voc = [token for token, _ in token_counter.most_common()[:VOCAB_MAX_SIZE]]
+    print token_voc
     token_voc.append(EMPTY_SYMBOL)
     return set(token_voc)
 
@@ -118,7 +119,6 @@ def process_corpus(corpus_path):
     #    print tv
     
     transformed_tokenized_sentences = get_transformed_tokenized_sentences(tokenized_sentences_for_transform,tokens_voc)
-    #transformed_tokenized_sentences = []
 
     index_to_token = dict(enumerate(tokens_voc))
 
@@ -139,35 +139,40 @@ def get_processed_sentence_and_index_to_token(corpus_path,processed_corpus_path=
 def get_nn_model(token_dict_size):
 
     '''model = Sequential()
-    #seq2seq = AttentionSeq2seq(
-    seq2seq = SimpleSeq2seq(
+    seq2seq = AttentionSeq2seq(
+    #seq2seq = SimpleSeq2seq(
         input_dim = TOKEN_REPRESENTATION_SIZE,
         input_length = INPUT_SEQUENCE_LENGTH,
         hidden_dim = HIDDEN_LAYER_DIMENSION,
         output_dim = token_dict_size,
-        output_length = ANSWER_MAX_TOKEN_LENGTH
+        output_length = ANSWER_MAX_TOKEN_LENGTH,
+        depth = 4
     )
     model.add(seq2seq)
-    model.compile(loss='categorical_crossentropy',optimizer='rmsprop')
+    model.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
     '''
+    dropout = 0.1 
     model = Sequential()
-    decoder_mode = 3
-    if decoder_mode == 3:
-        encoder_top_layer = LSTM(HIDDEN_LAYER_DIMENSION,input_dim=TOKEN_REPRESENTATION_SIZE,input_length=INPUT_SEQUENCE_LENGTH,return_sequences=True)
-    else:
-        encoder_top_layer = LSTM(HIDDEN_LAYER_DIMENSION)
+    encoder_top_layer = LSTM(HIDDEN_LAYER_DIMENSION,input_dim=TOKEN_REPRESENTATION_SIZE,input_length=INPUT_SEQUENCE_LENGTH,return_sequences=True)
 
     decoder_top_layer = AttentionDecoder(hidden_dim=HIDDEN_LAYER_DIMENSION,output_dim=HIDDEN_LAYER_DIMENSION,output_length=ANSWER_MAX_TOKEN_LENGTH,state_input=False,return_sequences=True)
     #model.add(Embedding(input_dim=TOKEN_REPRESENTATION_SIZE,output_dim=HIDDEN_LAYER_DIMENSION,input_length=INPUT_SEQUENCE_LENGTH))
     model.add(encoder_top_layer)
+    model.add(Dropout(dropout))
+    model.add(LSTM(HIDDEN_LAYER_DIMENSION,return_sequences=False))
+    model.add(RepeatVector(ANSWER_MAX_TOKEN_LENGTH))
     model.add(decoder_top_layer)
-    model.add(TimeDistributedDense(token_dict_size))
+    model.add(Dropout(dropout))
+    model.add(LSTM(HIDDEN_LAYER_DIMENSION,return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(TimeDistributed(Dense(token_dict_size)))
     model.add(Activation('softmax'))
 
     model.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
-
+    
     #if os.path.isfile(NN_MODEL_PATH):
     #    model.load_weights(NN_MODEL_PATH)
+
 
     return model
 
@@ -177,72 +182,66 @@ def get_nn_model(token_dict_size):
 
 '''
 
-def _batch(tokenized_sentences,batch_size=10):
+def _batch(tokenized_sentences,batch_size=1000):
     batch = []
     for line in tokenized_sentences:
         batch.append(line)
         if len(batch) == 2*batch_size:
-            yield batch
-            batch = []
+            #yield batch
+            #batch = []
+            break
 
-    yield []
+    return batch
+    #yield []
 
 def get_training_batch(w2v_model,tokenized_sentences,token_to_index):
 
     token_voc_size = len(token_to_index)
 
-    for sent_batch in _batch(tokenized_sentences,SAMPLES_BATCH_SIZE):
-        X = np.zeros((SAMPLES_BATCH_SIZE,INPUT_SEQUENCE_LENGTH,TOKEN_REPRESENTATION_SIZE))
-        Y = np.zeros((SAMPLES_BATCH_SIZE,ANSWER_MAX_TOKEN_LENGTH,token_voc_size))
-        sen_idx = 0
-        for s_idx in xrange(0,len(sent_batch),2):
+    X = np.zeros((SAMPLES_BATCH_SIZE,INPUT_SEQUENCE_LENGTH,TOKEN_REPRESENTATION_SIZE))
+    Y = np.zeros((SAMPLES_BATCH_SIZE,ANSWER_MAX_TOKEN_LENGTH,token_voc_size))
+    
+    #for sent_batch in _batch(tokenized_sentences,SAMPLES_BATCH_SIZE)
+    sent_batch = _batch(tokenized_sentences,SAMPLES_BATCH_SIZE)
+    sen_idx = 0
+    
+    for s_idx in xrange(0,len(sent_batch),2):
             
-            for t_idx,token in enumerate(sent_batch[s_idx][:INPUT_SEQUENCE_LENGTH]):
-                X[sen_idx,t_idx] = get_token_vector(token,w2v_model)
+        #print ('input:',sent_batch[s_idx][:INPUT_SEQUENCE_LENGTH])
+        #print ('output:',sent_batch[s_idx+1][:ANSWER_MAX_TOKEN_LENGTH])
 
-            for t_idx,token in enumerate(sent_batch[s_idx+1][:ANSWER_MAX_TOKEN_LENGTH]):
-                Y[sen_idx,t_idx,token_to_index[token]] = 1
-		
-	    sen_idx += 1    
-	yield X,Y
+        #print sent_batch[s_idx],sent_batch[s_idx+1]
+        for t_idx,token in enumerate(sent_batch[s_idx][:INPUT_SEQUENCE_LENGTH]):
+            X[sen_idx,t_idx] = get_token_vector(token,w2v_model)
+
+        for t_idx,token in enumerate(sent_batch[s_idx+1][:ANSWER_MAX_TOKEN_LENGTH]):
+            Y[sen_idx,t_idx,token_to_index[token]] = 1
+            
+        sen_idx += 1
+        #yield X,Y
+    return X,Y
 
 def train_model(nn_model,w2v_model,tokenized_sentences,index_to_token):
     print 'training nn model...'
-    token_voc_size = len(index_to_token)
+    #print ('Original input:',tokenized_sentences[0])
+    #print ('Expected output:',tokenized_sentences[1])
     token_to_index = dict(zip(index_to_token.values(),index_to_token.keys()))
+   
+    x_train,y_train = get_training_batch(w2v_model,tokenized_sentences,token_to_index)
     
     for full_data_pass_num in xrange(1,FULL_LEARN_ITER_NUM+1):
         
-        for x_train,y_train in get_training_batch(w2v_model,tokenized_sentences,token_to_index):
-           
-            '''print 'x_train[0]:',
-            i = 0
-            while i < INPUT_SEQUENCE_LENGTH:
-                print x_train[0][i]
-                i += 1
-            x_train[0]
-            i = 0
-            print ''
-            print 'y_train[0]:',
-            while i < ANSWER_MAX_TOKEN_LENGTH:
-                j = 0
-                while j < token_voc_size:
-                    if y_train[0,i,j] == 1:
-                        print index_to_token[j],
-                    j += 1
-                i += 1
-            print ''
-            raw_input('>:')'''
-            nn_model.fit(x_train,y_train,batch_size=TRAIN_BATCH_SIZE,nb_epoch=10,verbose=1)
-            '''our_predicts = nn_model.predict(x_train)
-            for i_idx,our_predict in enumerate(our_predicts):
-                predict_sequence = []
-                for predict_vector in our_predict:
-                    next_index = np.argmax(predict_vector)
-                    next_token = index_to_token[next_index]
-                    predict_sequence.append(next_token)
-                print ('Target output:',y_train[i_idx])
-                print ('Predict output:',predict_sequence)'''
+        #for x_train,y_train in get_training_batch(w2v_model,tokenized_sentences,token_to_index):
+        #    nn_model.fit(x_train,y_train,batch_size=TRAIN_BATCH_SIZE,nb_epoch=3,verbose=1)
+         nn_model.fit(x_train,y_train,batch_size=TRAIN_BATCH_SIZE,nb_epoch=10,verbose=1)
+         '''predictions = nn_model.predict(x_train);
+         for i_idx, predict in enumerate(predictions):
+             predict_sequence = []
+             for predict_vector in predict:
+                 next_index = np.argmax(predict_vector)
+                 next_token = index_to_token[next_index]
+                 predict_sequence.append(next_token)
+             print ('Predict output:',predict_sequence)'''
     nn_model.save_weights(NN_MODEL_PATH,overwrite=True)
     print 'end'
 
@@ -274,10 +273,12 @@ def predict_sentence(sentence,nn_model,w2v_model,index_to_token):
     input_sentence = tokenize(sentence+EOS_SYMBOL)[:INPUT_SEQUENCE_LENGTH]
     #print 'input_sentence:%s' % str(input_sentence)
     X = np.zeros((TRAIN_BATCH_SIZE,INPUT_SEQUENCE_LENGTH,TOKEN_REPRESENTATION_SIZE))
+    #print input_sentence
     for t, token in enumerate(input_sentence):
         X[0,t] = get_token_vector(token,w2v_model)
-    
-    predictions = nn_model.predict(X,verbose=0)[0]
+        #print X[0,t]
+       
+    predictions = nn_model.predict(X,verbose=1)[0]
     predicted_sequence = []
 
     for prediction_vector in predictions:
